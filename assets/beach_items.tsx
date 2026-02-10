f19654297167cdda93856f6e390cd12de7e25a96
theyashwanthsai <taddishetty34@gmail.com>
2026-02-10 16:41:14 +0530

    Remove all files

--- assets/beach_items.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="beach_items" tilewidth="16" tileheight="16" tilecount="360" columns="24">
- <image source="Farm RPG - Tiny Asset Pack - (All in One)/Exterior/Beach/Exterior Beach.png" width="384" height="240"/>
-</tileset>
--- assets/board.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="board" tilewidth="16" tileheight="16" tilecount="352" columns="32">
- <image source="Farm RPG - Tiny Asset Pack - (All in One)/Exterior/Exterior.png" width="512" height="176"/>
-</tileset>
